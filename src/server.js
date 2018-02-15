5b8ef115b0e669aa6cd7d3b5459d4b00887dd3a1
Samuel Cha <sunchipssam@gmail.com>
2018-02-15 15:12:41 -0800

    Deleted fs, refactored places/place per Ivan, made cache

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* IMPORTS ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const express = require('express');
 const fetch = require('node-fetch');
@@ -26,98 +25,134 @@ server.use(bodyParser.json());
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ DATA ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 let places = [];
-let place = [];
+let placesDetailed = [];
+const cache = {};
 
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* METHODS ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
-const writeToFile = _ => {
-  fs.writeFileSync('places.txt', JSON.stringify({ places }), 'utf8');
-};
-
-const deleteFile = _ => {
-  fs.writeFileSync('places.txt', JSON.stringify({ places: [] }), 'utf8');
-};
-
-const loadFile = _ => {
-  places = JSON.parse(fs.readFileSync('places.txt', 'utf8')).places;
-};
-
-const detailFetch = (res, req) => {
-  fetch(`${detailSearchURL}/${output}?placeid=${places[0].place_id}&key=${key}`)
+const detailFetch = placeId => {
+  console.log(placeId);
+  fetch(`${detailSearchURL}/${output}?placeid=${placeId}&key=${key}`)
     .then(response => response.json())
     .then(
       data =>
-        data.status === 'OK'
-          ? res.status(STATUS.OK).send(data.result.adr_address)
-          : res.status(STATUS.USER_ERROR).send(),
+        data.status === 'OK' ? place.push(data.result) : console.log(data),
     )
     .catch(err => console.log(err));
 };
 
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
-/* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ SCRIPTS ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
+/* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* SCRIPTS *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
-loadFile();
 
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* SERVER ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
-server.get('/places', (req, res) => {
-  res.status(STATUS.OK).send(
-    places.map(place => {
-      return `${place.name}: ${place.formatted_address}`;
-    }),
-  );
-});
+server.get('/place', (req, res) => {
+  const q = req.query.query;
 
-server.post('/places', (req, res) => {
-  const q = req.body.query;
-
-  if (!q) {
-    res.status(STATUS.USER_ERROR).send({ err: 'no query specified in body' });
+  if (cache[q] !== undefined) {
+    res.status(STATUS.OK).send(cache[q][0]);
     return;
   }
 
   fetch(`${textSearchURL}/${output}?query=${q}&key=${key}`)
     .then(response => response.json())
     .then(data => {
-      data.results.forEach(
-        place =>
-          places.filter(
-            placeCheck =>
-              placeCheck.formatted_address === place.formatted_address,
-          ).length > 0
-            ? null
-            : places.push(place),
-      );
-      writeToFile();
+      cache[q] = data.results;
+      res.status(STATUS.OK).send(data.results[0]);
     })
-    .then(res.status(STATUS.OK).send('Search successful.'))
     .catch(err => console.error(err));
 });
 
-server.get('/place', (req, res) => {
-  detailFetch(res, req);
-});
+server.get('/places', (req, res) => {
+  const q = req.query.query;
 
-server.post('/place', (req, res) => {
-  const q = req.body.query;
-
-  if (!q) {
-    detailFetch(res, req);
+  if (cache[q] !== undefined) {
+    res.status(STATUS.OK).send(cache[q]);
     return;
   }
 
-  console.log(q);
-  detailFetch(res, req);
+  fetch(`${textSearchURL}/${output}?query=${q}&key=${key}`)
+    .then(response => response.json())
+    .then(data => (cache[q] = data))
+    .catch(err => console.error(err));
 });
 
-server.delete('/places', (req, res) => {
-  places = [];
-  writeToFile();
-  res.status(STATUS.OK).send('Delete successful.');
-});
+// server.post('/places', (req, res) => {
+//   const q = req.body.query;
+
+//   if (!q) {
+//     res.status(STATUS.USER_ERROR).send({ err: 'no query specified in body' });
+//     return;
+//   }
+
+//   fetch(`${textSearchURL}/${output}?query=${q}&key=${key}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       places = [...data.results];
+//       places.forEach(place => {
+//         fetch(
+//           `${detailSearchURL}/${output}?placeid=${place.place_id}&key=${key}`,
+//         )
+//           .then(response => response.json())
+//           .then(data => placesDetailed.push(data.result))
+//           .catch(err => console.error(err));
+//       });
+//     })
+//     .then(_ => writeToFile())
+//     .then(_ => res.status(STATUS.OK).send('Search successful.'))
+//     .catch(err => console.error(err));
+// });
+
+// server.delete('/places', (req, res) => {
+//   places = [];
+//   writeToFile();
+//   res.status(STATUS.OK).send('Delete successful.');
+// });
+
+// // server.get('/place', (req, res) => {
+// //   res.status(STATUS.OK).send(
+// //     placesDetailed.map(place => {
+// //       return `open now: ${place.opening_hours.open_now} - ${
+// //         place.name
+// //       } - address: ${place.formatted_address}`;
+// //     }),
+// //   );
+// // });
+
+// server.post('/place', (req, res) => {
+//   places.forEach(placeObject => {
+//     fetch(
+//       `${detailSearchURL}/${output}?placeid=${placeObject.place_id}&key=${key}`,
+//     )
+//       .then(response => response.json())
+//       .then(data =>
+//         data.results.forEach(
+//           placeDetailed =>
+//             placeDetailed.filter(
+//               placeCheck =>
+//                 placeCheck.formatted_address ===
+//                 placeDetailed.formatted_address,
+//             ).length > 0
+//               ? null
+//               : place.push(placeDetailed),
+//         ),
+//       )
+//       .then(_ => writeToFile())
+//       .then(_ =>
+//         res.status(STATUS.OK).send('Places details added succesfully.'),
+//       )
+//       .catch(err => console.log(err));
+//   });
+// });
+
+// server.delete('/place', (req, res) => {
+//   placesDetailed = [];
+//   writeToFile();
+//   res.status(STATUS.OK).send('Delete successful.');
+// });
 
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* LISTEN ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
