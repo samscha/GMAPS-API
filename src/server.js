2449f8b4a12408a5656e5941bfd73c5345c7e1e7
Samuel Cha <sunchipssam@gmail.com>
2018-02-15 20:05:37 -0800

    Refactored into methods

--- src/server.js
+++ src/server.js
@@ -30,6 +30,38 @@ const cache = {};
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* METHODS ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* */
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
+const textSearchSearch = (TEXTSEARCH_URL, res) => {
+  if (cache[TEXTSEARCH_URL] !== undefined) {
+    res.status(STATUS.OK).send(cache[TEXTSEARCH_URL]);
+    return;
+  }
+
+  fetchData(TEXTSEARCH_URL, res);
+};
+
+const detailsSearch = (TEXTSEARCH_URL, res) => {
+  if (cache[TEXTSEARCH_URL] !== undefined) {
+    const placeId = cache[TEXTSEARCH_URL][0].place_id;
+    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
+
+    if (cache[DETAILS_URL] !== undefined) {
+      res.status(STATUS.OK).send(cache[DETAILS_URL]);
+      return;
+    }
+
+    fetchData(DETAILS_URL, res);
+    return;
+  }
+
+  fetchData(TEXTSEARCH_URL).then(_ => {
+    const placeId = cache[TEXTSEARCH_URL][0].place_id;
+    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
+
+    fetchData(DETAILS_URL, res);
+    return;
+  });
+};
+
 const fetchData = (URL, res) => {
   return fetch(`${URL}`)
     .then(response => response.json())
@@ -53,76 +85,28 @@ server.get('/places', (req, res) => {
   const q = req.query.query.split(' ').join('+');
   const TEXTSEARCH_URL = `${textSearchBaseURL}&query=${q}`;
 
-  if (cache[TEXTSEARCH_URL] !== undefined) {
-    res.status(STATUS.OK).send(cache[TEXTSEARCH_URL]);
-    return;
-  }
-
-  fetchData(TEXTSEARCH_URL, res);
+  textSearchSearch(TEXTSEARCH_URL, res);
 });
 
 server.get('/place', (req, res) => {
   const q = req.query.query.split(' ').join('+');
   const TEXTSEARCH_URL = `${textSearchBaseURL}&query=${q}`;
 
-  if (cache[TEXTSEARCH_URL] !== undefined) {
-    const placeId = cache[TEXTSEARCH_URL][0].place_id;
-    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
-
-    if (cache[DETAILS_URL] !== undefined) {
-      res.status(STATUS.OK).send(cache[DETAILS_URL]);
-      return;
-    }
-
-    fetchData(DETAILS_URL, res);
-    return;
-  }
-
-  fetchData(TEXTSEARCH_URL).then(_ => {
-    const placeId = cache[TEXTSEARCH_URL][0].place_id;
-    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
-
-    fetchData(DETAILS_URL, res);
-    return;
-  });
+  detailsSearch(TEXTSEARCH_URL, res);
 });
 
 server.get('/airports', (req, res) => {
   const q = req.query.query.split(' ').join('+');
   const TEXTSEARCH_URL = `${textSearchBaseURL}&query=${q}&type=airport`;
 
-  if (cache[TEXTSEARCH_URL]) {
-    res.status(STATUS.OK).send(cache[TEXTSEARCH_URL]);
-    return;
-  }
-
-  fetchData(TEXTSEARCH_URL, res);
+  textSearchSearch(TEXTSEARCH_URL, res);
 });
 
 server.get('/airport', (req, res) => {
   const q = req.query.query.split(' ').join('+');
   const TEXTSEARCH_URL = `${textSearchBaseURL}&query=${q}&type=airport`;
 
-  if (cache[TEXTSEARCH_URL] !== undefined) {
-    const placeId = cache[TEXTSEARCH_URL][0].place_id;
-    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
-
-    if (cache[DETAILS_URL] !== undefined) {
-      res.status(STATUS.OK).send(cache[DETAILS_URL]);
-      return;
-    }
-
-    fetchData(DETAILS_URL, res);
-    return;
-  }
-
-  fetchData(TEXTSEARCH_URL).then(_ => {
-    const placeId = cache[TEXTSEARCH_URL][0].place_id;
-    const DETAILS_URL = `${detailedSearchBaseURL}&placeid=${placeId}`;
-
-    fetchData(DETAILS_URL, res);
-    return;
-  });
+  detailsSearch(TEXTSEARCH_URL, res);
 });
 
 /* ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ */
